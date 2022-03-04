35c3745754e99f92e595594f9ad53997cf246acc
Simple <varg616@gmail.com>
2022-03-04 22:10:49 +0300

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Form from "./components/Form.js";
-import mailImg from "./img/digitalmail.jpg";
 
 function App() {
     return (
         <div className="App">
             <div className="global__container">
-                {/* <img src={mailImg} alt="Neon mail" /> */}
                 <Form />
             </div>
         </div>
